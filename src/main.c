46691ef38331c503669d48943a0ffc8e8e0452cf
aqua-ytw <migonoodleaqua@gmail.com>
2023-07-17 15:25:34 +0200

    completed challenge 2

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <stdint.h>
+#include "espl_lib.h"
 
 int main() {
-	
-    printf("Hello ESPL \n");
+
+    int exit = 0;
+    do
+    {
+        uint32_t num; 
+        char *return_str;
+        printf("Please enter a number: ");
+        scanf("%d", &num);
+
+        return_str = num_to_words(num);
+        printf("The return string is %s\n", return_str);
+        
+
+        printf("Enter 0 for exit, 1 for continue: ");
+        scanf("%d", &exit);
+
+
+    } while (exit);
     return 0;
+   
 }
